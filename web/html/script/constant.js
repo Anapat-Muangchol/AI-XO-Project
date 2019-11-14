2acec0e89088cfee36444dcf81d5e51901aa0ae2
anapat muangchol <005775@cdg.co.th>
2019-11-14 09:23:54 +0700

    resolve https api can't connect

--- web/html/script/constant.js
+++ web/html/script/constant.js
@@ -1,4 +1,5 @@
 // var API_HOSTNAME = 'http://10.0.30.11:3000'
 // var API_HOSTNAME = 'http://10.0.30.11:13000'
 // var API_HOSTNAME = 'http://localhost:13000'
-var API_HOSTNAME = 'https://203.150.243.33:3000'
+var API_HOSTNAME = 'http://203.150.243.33:3000'
+// var API_HOSTNAME = 'https://203.150.243.33:3000'
