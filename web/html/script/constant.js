ef7aa9ffd9fa1cb0503fb04f2492f2b40cc2e29e
anapat muangchol <005775@cdg.co.th>
2019-11-14 08:20:46 +0700

    Change api url for deploy

--- web/html/script/constant.js
+++ web/html/script/constant.js
@@ -1,3 +1,4 @@
 // var API_HOSTNAME = 'http://10.0.30.11:3000'
 // var API_HOSTNAME = 'http://10.0.30.11:13000'
-var API_HOSTNAME = 'http://localhost:13000'
+// var API_HOSTNAME = 'http://localhost:13000'
+var API_HOSTNAME = 'http://203.150.243.33:3000'
